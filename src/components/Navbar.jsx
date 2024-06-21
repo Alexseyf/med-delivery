9728c34f7526f2d62bdc31caf0d464d7bb2382b0
Alexseyf <alexseyf66@gmail.com>
2024-06-21 09:46:01 -0300

    tailwind implementado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
       }}>
         <CiMenuFries className="lg:hidden sm:flex fixed top-0 right-0 z-10 mr-2 mt-2 text-2xl "/>
       </div>
-      <ul className={openMenu ? "flex-col fixed top-0 bg-slate-100 w-full text-lg" : "hidden gap-4  md:flex md:fixed md: bg-slate-100 top-0 md:w-full text-lg"}>
+      <ul className={openMenu ? "flex-col fixed top-0 bg-primary text-white w-full text-lg" : "hidden gap-4  md:flex md:fixed md:bg-primary text-white top-0 md:w-full text-lg"}>
        
         <li className="ml-2 mt-2">
           <NavLink to="/inicio">Início</NavLink>
